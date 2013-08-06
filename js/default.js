3d79b29a71b73e9874ddb767b73d0b30532b9ef5
Breno Moura <basmoura@gmail.com>
2013-08-06 00:55:40 -0300

    alteracoes

--- js/default.js
+++ js/default.js
@@ -1,14 +1,65 @@
-$(document).ready(function () {
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 101) {
-			$('.nav-container').addClass("f-nav logosmall");
-			alert($(this).attr('.nav-container'))
-		} else {
-			$('.nav-container').removeClass("f-nav logosmall");
-		}
-	});
-	$("a.anchor").anchorAnimate({offset:60});
+// $(document).ready(function () {
+// 	$(window).scroll(function () {
+// 		if ($(this).scrollTop() > 101) {
+// 			$('.nav-container').addClass("f-nav logosmall");
+// 			alert($(this).attr('.nav-container'))
+// 		} else {
+// 			$('.nav-container').removeClass("f-nav logosmall");
+// 		}
+// 	});
+// 	$("a.anchor").anchorAnimate({offset:60});
+// });
+$(function () {
+	// $('.nav-container').addClass("f-nav logosmall");
+	var lastId,
+	topMenu = $(".nav"),
+	topMenuHeight = topMenu.outerHeight()+44,
+
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+    	var item = $($(this).attr("href"));
+    	if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+	var href = $(this).attr("href"),
+	offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+	$('html, body').stop().animate({ 
+		scrollTop: offsetTop
+	}, 800);
+	e.preventDefault();
 });
 
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   if (fromTop > 159) {	
+   	$('.nav-container').addClass("f-nav logosmall");
+   } else {
+   	$('.nav-container').removeClass("f-nav logosmall");
+   }
 
-
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+   	if ($(this).offset().top < fromTop)
+   		return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+   	lastId = id;
+       // Set/remove active class
+       menuItems
+       .parent().removeClass("active")
+       .end().filter("[href=#"+id+"]").parent().addClass("active");
+     }                   
+   });
+});
--- js/jquery.anchor.js
+++ js/jquery.anchor.js
@@ -15,32 +15,32 @@
 	*****/
 
 
-	jQuery.fn.anchorAnimate = function(settings) {
+	// jQuery.fn.anchorAnimate = function(settings) {
 
-		settings = jQuery.extend({
-			speed : 1000,
-			offset: 0
-		}, settings);	
+	// 	settings = jQuery.extend({
+	// 		speed : 1000,
+	// 		offset: 0
+	// 	}, settings);	
 
-		return this.each(function(){
-			var caller = this
-			$(caller).click(function (event) {	
-				event.preventDefault();
-				event.returnValue = false;
-				var locationHref = window.location.href;
-				var elementClick = $(caller).attr("href");
-				var destination = $(elementClick).offset().top - settings.offset;
-				$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, settings.speed, function() {
-				scrollPosition = $(window).scrollTop(); // Firefox hack
-				window.location.hash = elementClick;
-				$(window).scrollTop(scrollPosition); // Firefox hack
+	// 	return this.each(function(){
+	// 		var caller = this
+	// 		$(caller).click(function (event) {	
+	// 			event.preventDefault();
+	// 			event.returnValue = false;
+	// 			var locationHref = window.location.href;
+	// 			var elementClick = $(caller).attr("href");
+	// 			var destination = $(elementClick).offset().top - settings.offset;
+	// 			$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, settings.speed, function() {
+	// 			scrollPosition = $(window).scrollTop(); // Firefox hack
+	// 			window.location.hash = elementClick;
+	// 			$(window).scrollTop(scrollPosition); // Firefox hack
 
-			});
-				return false;
-			})
-		})
-	}
+	// 		});
+	// 			return false;
+	// 		})
+	// 	})
+	// }
 
-	$(document).ready(function() {
+	// $(document).ready(function() {
 		
-	});
+	// });
